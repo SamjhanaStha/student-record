0c978b49d55be9e23fb12aad8d3a0d080cf919ec
SamjhanaStha <smjhnashrestha16@gmail.com>
2026-06-23 06:52:56 +0545

    middleware add

--- src/server.js
+++ src/server.js
@@ -5,11 +5,34 @@ import departmentRouter from "./routes/department_routes.js"
 import teacherRouter from "./routes/Teacher_routes.js"
 import courseRouter from "./routes/course_routes.js"
 import enrollmentRouter from "./routes/enrollment_routes.js"
+import { checkXApiKeyInHeader, checkXRoleHeaderMiddleware } from "./middlewares/header_middlerware.js"
 // github.com/DipakShrestha-ADS/student_record.git
 dotenv.config()
 let app = express()
 app.use(express.json())
+
+// custom middleware
+app.use((req, res, next) => {
+    console.log(`req Url ${req.url}`)
+    if(req.url == "/"){
+        res.json({
+            errorMsg: "this url cannot be access"
+        })
+        return
+    }
+    next()
+})
  
+app.use(checkXRoleHeaderMiddleware)
+// route level middleware
+app.use("/apikey",
+    checkXApiKeyInHeader, (req,res)=>{
+    res.status(200).json({
+        message: "api key called",
+        data: req.headers["x-api-key"]
+    })
+})
+
 app.use("/students", studentRouter)
 app.use("/departments", departmentRouter)
 app.use("/teachers", teacherRouter)
